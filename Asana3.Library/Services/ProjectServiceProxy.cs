6a4de901c68843b6b38f8833e1ab4e337539b352
sarahfieg04 <sarahfieg@icloud.com>
2026-03-10 02:40:23 -0400

    Add files via upload

---
+++ Asana3.Library/Services/ProjectServiceProxy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Asana3.Maui.Util;
+using Newtonsoft.Json;
+using Asana3.Library.Models;
+
+namespace Asana3.Library.Services
+{
+    public class ProjectServiceProxy
+    {
+        private List<Project> projects;
+
+        public List<Project> Projects
+        {
+            get { return projects; }
+        }
+
+        private ProjectServiceProxy()
+        {
+            var projectData = new WebRequestHandler().Get("/Project/Expand").Result;
+            // projects = JsonConvert.DeserializeObject<List<Project>>(projectData) ?? new List<Project>();
+            if (string.IsNullOrWhiteSpace(projectData))
+            {
+                projects = new List<Project>();
+            }
+            else
+            {
+                projects = JsonConvert.DeserializeObject<List<Project>>(projectData) ?? new List<Project>();
+            }
+        }
+
+        private static object _lock = new object();
+        private static ProjectServiceProxy? instance;
+
+        public static ProjectServiceProxy Current
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ProjectServiceProxy();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public Project? AddOrUpdate(Project? project)
+        {
+            if (project == null) return null;
+
+            var existing = projects.FirstOrDefault(p => p.Id == project.Id);
+
+            if (existing != null)
+            {
+                existing.Name = project.Name;
+                existing.Description = project.Description;
+                existing.CompletePercent = project.CompletePercent;
+            }
+            else
+            {
+                project.Id = projects.Any() ? projects.Max(p => p.Id) + 1 : 1;
+                projects.Add(project);
+            }
+
+            return project;
+        }
+
+        public void DisplayProjects()
+        {
+            projects.ForEach(Console.WriteLine);
+        }
+
+        public Project? GetById(int id)
+        {
+            return projects.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void DeleteProject(Project? project)
+        {
+            if (project != null)
+            {
+                projects.Remove(project);
+            }
+        }
+
+        public void ToDosInProject(Project? project)
+        {
+            if (project == null) return;
+
+            project.ToDos.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ Asana3/Program.cs
@@ -0,0 +1,177 @@
+﻿using Asana3.Library.Models;
+using Asana3.Library.Services;
+
+namespace Asana3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var toDoSvc = ToDoServiceProxy.Current;
+            var projectSvc = ProjectServiceProxy.Current;
+            int choiceInt;
+
+            do
+            {
+                Console.WriteLine("\nChoose a menu option:");
+                Console.WriteLine("1. Create a ToDo");
+                Console.WriteLine("2. List all ToDos");
+                Console.WriteLine("3. List all outstanding ToDos");
+                Console.WriteLine("4. Delete a ToDo");
+                Console.WriteLine("5. Update a ToDo");
+                Console.WriteLine("6. Create a Project");
+                Console.WriteLine("7. List all Projects");
+                Console.WriteLine("8. Update a Project");
+                Console.WriteLine("9. Delete a Project");
+                Console.WriteLine("10. List all ToDos in a Project");
+                Console.WriteLine("11. Add a ToDo to a Project");
+                Console.WriteLine("12. Exit");
+
+                var choice = Console.ReadLine() ?? "12";
+
+                if (int.TryParse(choice, out choiceInt))
+                {
+                    switch (choiceInt)
+                    {
+                        case 1:
+                            Console.Write("Name: ");
+                            var name = Console.ReadLine();
+                            Console.Write("Description: ");
+                            var description = Console.ReadLine();
+                            toDoSvc.AddOrUpdate(new ToDo
+                            {
+                                Name = name,
+                                Description = description,
+                                IsCompleted = false,
+                                Id = 0
+                            });
+                            break;
+
+                        case 2:
+                            toDoSvc.DisplayToDos(true);
+                            break;
+
+                        case 3:
+                            toDoSvc.DisplayToDos();
+                            break;
+
+                        case 4:
+                            toDoSvc.DisplayToDos(true);
+                            Console.Write("ToDo to Delete: ");
+                            var toDoChoice4 = int.Parse(Console.ReadLine() ?? "0");
+                            var reference = toDoSvc.GetById(toDoChoice4);
+                            toDoSvc.DeleteToDo(reference);
+                            break;
+
+                        case 5:
+                            toDoSvc.DisplayToDos(true);
+                            Console.Write("ToDo to Update: ");
+                            var toDoChoice5 = int.Parse(Console.ReadLine() ?? "0");
+                            var updateReference = toDoSvc.GetById(toDoChoice5);
+                            if (updateReference != null)
+                            {
+                                Console.Write("Name: ");
+                                updateReference.Name = Console.ReadLine();
+                                Console.Write("Description: ");
+                                updateReference.Description = Console.ReadLine();
+                                toDoSvc.AddOrUpdate(updateReference);
+                            }
+                            break;
+
+                        case 6:
+                            Console.Write("Name: ");
+                            var projectName = Console.ReadLine();
+                            Console.Write("Description: ");
+                            var projectDescription = Console.ReadLine();
+                            projectSvc.AddOrUpdate(new Project
+                            {
+                                Name = projectName,
+                                Description = projectDescription,
+                                CompletePercent = 0,
+                                Id = 0
+                            });
+                            break;
+
+                        case 7:
+                            projectSvc.DisplayProjects();
+                            break;
+
+                        case 8:
+                            Console.WriteLine("Choose a project to update: ");
+                            projectSvc.DisplayProjects();
+                            var toDoChoice8 = int.Parse(Console.ReadLine() ?? "0");
+                            var reference8 = projectSvc.GetById(toDoChoice8);
+                            if (reference8 != null)
+                            {
+                                Console.Write("Name: ");
+                                reference8.Name = Console.ReadLine();
+                                Console.Write("Description: ");
+                                reference8.Description = Console.ReadLine();
+                                projectSvc.AddOrUpdate(reference8);
+                            }
+                            break;
+
+                        case 9:
+                            Console.WriteLine("Choose a project to delete: ");
+                            projectSvc.DisplayProjects();
+                            var toDoChoice9 = int.Parse(Console.ReadLine() ?? "0");
+                            var reference9 = projectSvc.GetById(toDoChoice9);
+                            if (reference9 != null)
+                            {
+                                projectSvc.DeleteProject(reference9);
+                            }
+                            break;
+
+                        case 10:
+                            Console.Write("Choose a project to display: ");
+                            projectSvc.DisplayProjects();
+                            var toDoChoice10 = int.Parse(Console.ReadLine() ?? "0");
+                            var reference10 = projectSvc.GetById(toDoChoice10);
+                            if (reference10 != null)
+                            {
+                                projectSvc.ToDosInProject(reference10);
+                            }
+                            break;
+
+                        case 11:
+                            Console.Write("Project to add ToDo to: ");
+                            projectSvc.DisplayProjects();
+                            var toDoChoice11 = int.Parse(Console.ReadLine() ?? "0");
+                            var reference11 = projectSvc.GetById(toDoChoice11);
+
+                            if (reference11 != null)
+                            {
+                                Console.Write("Name: ");
+                                var name11 = Console.ReadLine();
+                                Console.Write("Description: ");
+                                var description11 = Console.ReadLine();
+                                var newToDo = new ToDo
+                                {
+                                    Name = name11,
+                                    Description = description11,
+                                    IsCompleted = false,
+                                    Id = 0
+                                };
+                                reference11.ToDos.Add(newToDo);
+                            }
+                            break;
+
+                        case 12:
+                            Console.WriteLine("Exiting program...");
+                            break;
+
+                        default:
+                            Console.WriteLine("ERROR: Unknown menu selection");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR: {choice} is not a valid menu selection");
+                    choiceInt = -1; // continue loop
+                }
+
+            } while (choiceInt != 12);
+        }
+    }
+}
